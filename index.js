320496a20f1e93bc249ecf188e00e5baa6116b12
Your Name <you@example.com>
2022-12-08 19:03:19 +0300

    done

--- index.js
+++ index.js
@@ -15,11 +15,32 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
+  console.log("racecar");
+  
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("robot");
+  
 }
+if (require.main === module) {
+ // add your own custom tests in here
+
 
 module.exports = isPalindrome;
+
+console.log("Expecting:true")
+console.log("=>", isPalindrome("dad"));
+console.log("dad")
+}
+
+if (require.main === module) {
+ // add your own custom tests in here
+
+module.exports = isPalindrome;
+console.log("Expecting:false")
+console.log("=>", isPalindrome("casual"))
+console.log("casual");
+};
+
+
